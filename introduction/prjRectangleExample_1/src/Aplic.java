ca992a4402a15bdcca6a9066419d5fcefed3c632
Henrique A. R. <henrique4r.it@gmail.com>
2025-08-20 10:51:00 -0300

    study: Implement setters to encapsulate data

--- introduction/prjRectangleExample_1/src/Aplic.java
+++ introduction/prjRectangleExample_1/src/Aplic.java
@@ -5,7 +5,16 @@
 public class Aplic {
 
     public static void main(String[] args) {
+        Rectangle retObj; //definição de ponteiro
         
+        retObj = new Rectangle(); //instanciação da classe
+        
+        retObj.setHeight(5.0); //atribuição de valores 
+        retObj.setWidth(8.0); // ao objeto através de métodos
+        
+        System.out.println("Medida da area: " + retObj.calcArea()); //Envia os dados à tela
+        
+        System.out.println("Medida do perimetro: " + retObj.calcPerimeter()); //Envia os dados à tela
     }
     
 }
