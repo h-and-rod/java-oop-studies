c0c9e52624088d1a67049b1efa06e18e01985333
Henrique A. R. <henrique4r.it@gmail.com>
2025-08-26 23:08:59 -0300

    feat(prjStudentExercise): implements Student gets and sets

--- introduction/prjStudentExercise/src/Student.java
+++ introduction/prjStudentExercise/src/Student.java
@@ -11,4 +11,52 @@ public class Student {
     private double Prj1Gd;
     private double Prj2Gd;
 
+    public void setRA(int ra) {
+        RA = ra;
+    }
+
+    public void setTst1Gd(double grade){
+        Tst1Gd = grade;
+    }
+
+    public void setTst2Gd(double grade){
+        Tst2Gd = grade;
+    }
+
+    public void setPrj1Gd(double grade){
+        Prj1Gd = grade;
+    }
+
+    public void setPrj2Gd(double grade){
+        Prj2Gd = grade;
+    }
+
+    public int getRA() {
+        return RA;
+    }
+
+    public double getTst1Gd() {
+        return Tst1Gd;
+    }
+
+    public double getTst2Gd() {
+        return Tst2Gd;
+    }
+
+    public double getPrj1Gd() {
+        return Prj1Gd;
+    }
+
+    public double getPrj2Gd() {
+        return Prj2Gd;
+    }
+
+    public double calcTstAverage() {
+        double average;
+
+        average = 0.75 * ((Tst1Gd + 2 * Tst2Gd)/3);
+
+        return average;
+    }
+
 }
